c47fb7bde36c554252d25a4fa5ba17171e56b0d4
trinoeg8@gmail.com <trinoeg8@trinux.trinux.d3.internal.cloudapp.net>
2013-12-29 04:08:30 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DemoApp::Application.config.secret_key_base = '81a434034e5a517bce4fd16516fdaeece7e6596387727c7027d9405e458874acabd461c24520d7ad42ae45a15b0872a84b9eacffd5ead22f158c9704b655b22c'
