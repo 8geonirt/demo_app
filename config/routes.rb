813a42b42c05c40fb36fa628aaa15e2b1eda4851
azbycxdw1029384756 <azbycxdw1029384756@linux-n18l.site>
2013-12-30 00:35:16 -0600

    Finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,15 @@
 DemoApp::Application.routes.draw do
-  get "static_pages/home"
-  get "static_pages/help"
-  get "static_pages/about"
-  get "static_pages/contact"
+  #get "static_pages/home"
+  #get "static_pages/help"
+  #match '/', to: 'static_pages#home', via: 'get'
+  get "users/new"
+  match '/signup',  to: 'users#new',            via: 'get'
+  root  'static_pages#home'
+  match '/help',    to: 'static_pages#help',    via: 'get'
+  match '/about',   to: 'static_pages#about',   via: 'get'
+  match '/contact', to: 'static_pages#contact', via: 'get'
+  #get "static_pages/about"
+  #get "static_pages/contact"
   resources :microposts
 
   resources :users
